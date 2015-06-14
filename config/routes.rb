ebc4e5da117998a0b3787ea39b4114603d08d589
Steven Holt <steven@scorbord.com>
2015-06-14 08:44:57 -0400

    adjust css, make PracticePeriods the Root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root                  'static_pages#home'
+  root                  'practice_plans#index'
   get     'help'      =>  'static_pages#help'
   get     'about'     =>  'static_pages#about'
   get     'contact'   =>  'static_pages#contact'
