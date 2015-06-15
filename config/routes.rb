11298d75fa8c1fd679ae6c916fec127425e62f4f
Steven Holt <steven@scorbord.com>
2015-06-15 16:42:16 -0400

    Fixed Home Page to load Static Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root                  'practice_plans#index'
+  root                  'static_pages#home'
   get     'help'      =>  'static_pages#help'
   get     'about'     =>  'static_pages#about'
   get     'contact'   =>  'static_pages#contact'
